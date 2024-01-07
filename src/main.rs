9c258a6779e29773e2574d2c3de578fd0ebd4cfb
Nando <nandolawson@gmail.com>
2024-01-07 15:17:23 +0100

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -29,47 +29,47 @@ fn main() {
 fn generate_product_key() -> String {
 	let mut rng = rand::thread_rng();
 	loop {
-		let block_a: usize = (0..3)
+		let block_a: u32 = (0..3)
 			.map(|_| rng
 				.gen_range('0'..='9')
 				.to_digit(10)
-				.unwrap_or_default() as usize
+				.unwrap_or_default()
 			)
 			.fold(0, |acc, digit| acc * 10 + digit);
 		if !(block_a % 111 == 0 && block_a >= 3 && block_a <= 9) {
-			let block_b: usize = (0..7)
+			let block_b: u32 = (0..7)
 			.map(|_| rng
 				.gen_range('0'..='8')
 				.to_digit(10)
-				.unwrap_or_default() as usize
+				.unwrap_or_default()
 			)
 			.fold(0, |acc, digit| acc * 10 + digit);
-			let block_b_numbers: Vec<u32> = block_b.to_string()
+			let block_b_check: Vec<u32> = block_b
+				.to_string()
 				.chars()
 				.map(|c| c
 					.to_digit(10)
 					.unwrap()
 				)
 				.collect();
-			if block_b_numbers
+			if block_b_check
 				.iter()
 				.sum::<u32>() % 7 == 0 {
-				let product_key = format!("{}-{}", block_a, block_b);
+				let product_key: String = format!("{}-{}", block_a, block_b);
 				return product_key;
 			}
 		}
 	}
 }
 fn validate_product_key(product_key: &str) -> bool {
-	let block_a: usize = product_key[0..3]
+	let block_a: u32 = product_key[0..3]
 		.parse()
 		.unwrap_or_default();
-	let block_b: usize = product_key[4..]
+	let block_b: u32 = product_key[4..]
     	.chars()
     	.filter_map(|c| c
 			.to_digit(10)
 		)
-    	.map(|d| d as usize)
     	.sum();
 	if (product_key
 			.len() != 11
