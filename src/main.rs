db4ca5a0656f761d084673716dfe18b35a156f63
Nando <nandolawson@gmail.com>
2024-01-07 14:40:32 +0100

    Code cleanup and small fixes in Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ fn main() {
 		.collect();
 	println!("Win95 Key Tool");
 	// Check how many arguments are available
-	if args.len() != 2 {
+	if args
+		.len() != 2 {
 		eprintln!("Usage: {} <product key> / generate", args[0]);
 		std::process::exit(1);
 	}
 	// Load argument into variable and check if it is called "generate"
 	// Everything else will be checked if it is a valid product key
-	let product_key: &str = args[1].as_str();
+	let product_key: &str = args[1]
+		.as_str();
 	if product_key == "generate" {
 		println!("Windows 95 key: {}", generate_product_key());
 	}
@@ -27,23 +29,31 @@ fn main() {
 fn generate_product_key() -> String {
 	let mut rng = rand::thread_rng();
 	loop {
-		let block_a: String = (0..3)
+		let block_a: usize = (0..3)
 			.map(|_| rng
-				.gen_range('0'..='9'))
-			.collect();
-		if !["333", "444", "555", "666", "777", "888", "999"]
-			.concat()
-			.contains(&block_a) {
-			let block_b: String = (0..7)
-				.map(|_| rng.gen_range('0'..='8'))
-				.collect();
-			let block_b_numbers: Vec<u32> = block_b
+				.gen_range('0'..='9')
+				.to_digit(10)
+				.unwrap_or_default() as usize
+			)
+			.fold(0, |acc, digit| acc * 10 + digit);
+		if !(block_a % 111 == 0 && block_a >= 3 && block_a <= 9) {
+			let block_b: usize = (0..7)
+			.map(|_| rng
+				.gen_range('0'..='8')
+				.to_digit(10)
+				.unwrap_or_default() as usize
+			)
+			.fold(0, |acc, digit| acc * 10 + digit);
+			let block_b_numbers: Vec<u32> = block_b.to_string()
 				.chars()
 				.map(|c| c
 					.to_digit(10)
-				.unwrap())
+					.unwrap()
+				)
 				.collect();
-			if block_b_numbers.iter().sum::<u32>() % 7 == 0 {
+			if block_b_numbers
+				.iter()
+				.sum::<u32>() % 7 == 0 {
 				let product_key = format!("{}-{}", block_a, block_b);
 				return product_key;
 			}
@@ -51,28 +61,26 @@ fn generate_product_key() -> String {
 	}
 }
 fn validate_product_key(product_key: &str) -> bool {
-	if product_key.len() != 11 {
-		return false;
-	}
-	let block_a: usize = product_key[0..3].parse().unwrap_or_default();
-    if block_a % 111 == 0 {
-        if block_a >= 3 && block_a <= 9 {
-			return false;
-		}
-	}
-	if product_key.chars().nth(3) != Some('-') {
-		return false;
-	}
-	let block_b: String = product_key[4..]
+	let block_a: usize = product_key[0..3]
 		.parse()
 		.unwrap_or_default();
-	let sum: usize = block_b
-		.chars()
-		.filter_map(|c| c
-				.to_digit(10))
-		.map(|d| d as usize)
-		.sum();
-	if sum % 7 != 0 {
+	let block_b: usize = product_key[4..]
+    .parse::<usize>()
+    .unwrap_or_default()
+    .to_string()
+    .chars()
+    .filter_map(|c| c
+		.to_digit(10)
+	)
+    .map(|d| d as usize)
+    .sum();
+	if (product_key
+			.len() != 11) ||
+		(block_a % 111 == 0 && block_a >= 3 && block_a <= 9) ||
+		(product_key
+			.chars()
+			.nth(3) != Some('-')) ||
+		(block_b % 7 != 0) {
 		return false;
 	}
 	true
