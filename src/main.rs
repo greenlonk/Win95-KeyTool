4684944d0fb81c245f760ccac5de383f9df7c1dd
Max <23058595+greenlonk@users.noreply.github.com>
2024-01-10 16:45:01 +0100

    Code rewrite and optimization

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,89 @@
-/*Dependencies*/
-use std::env;
 use rand::Rng;
+use std::env;
+use std::process::exit;
 
-/*Code*/
 fn main() {
-	// Get arguments from terminal
-	let args: Vec<String> = env::args()
-		.collect();
-	println!("[Win95 Key Tool]");
-	// Check how many arguments are available
-	if args
-		.len() != 2 {
-		eprintln!("Usage: {} <product key> / generate", args[0]);
-		std::process::exit(1);
+	let args: Vec<String> = env::args().collect();
+	if args.len() != 2 || !check_key_length(&args[1]) {
+		println!("Usage: {} <product_key / generate>", args[0]);
+		println!("Example: win95 XXX-XXXXXXX");
+		exit(1);
 	}
-	// Load argument into variable and check if it is called "generate"
-	// Everything else will be checked if it is a valid product key
-	let product_key: &str = args[1]
-		.as_str();
+
+	let product_key: String = String::from(&args[1]);
 	if product_key == "generate" {
-		println!("Windows 95 key: {}", generate_product_key());
+		let key = generate_product_key();
+		println!(
+			"Windows 95 Key: {}-{}",
+			format!("{:03}", key[0]),
+			format!("{:07}", key[1])
+		);
+	} else {
+		if validate_product_key(parse_input(&product_key)) {
+			println!("Valid key: {}", product_key);
+		} else {
+			println!("Invalid key: {}", product_key);
+		}
 	}
-	else if validate_product_key(product_key) {
-		println!("Valid key: {}", product_key);
+
+	fn check_key_length(input: &String) -> bool {
+		if input.trim().len() == 11 || input.trim().len() == 8 {
+			return true;
+		} else {
+			false
+		}
 	}
-	else {
-		println!("Invalid key: {}", product_key);
+
+	fn parse_input(input: &String) -> Vec<usize> {
+		let key: Vec<usize>;
+
+		key = vec![
+			input[0..3].trim().parse().expect("Not a number!\n"),
+			input[4..11].trim().parse().expect("Not a number!\n"),
+		];
+		key
 	}
-}
-fn generate_product_key() -> String {
-	let mut rng = rand::thread_rng();
-	loop {
-		// Generate first block of the product key
-		let block_a: u32 = rng
-			.gen_range(100..=998);
-		// Check if it matches all rules
-		if !(((3..=9).contains(&(block_a / 111))) && block_a % 111 == 0) {
-			loop {
-				// Generate second block of the product key
-				let block_b: u32 = rng
-					.gen_range(1000000..=8888888);
-				// Check if it matches all rules
-				if block_b
-					.to_string()
-					.chars()
-					.filter_map(|c| c
-						.to_digit(10)
-					)
-					.sum::<u32>() % 7 == 0 {
-					// Merge both blocks and send it back to main function
-					let product_key: String = format!("{}-{}", block_a, block_b);
-					return product_key;
-				}
+	fn generate_product_key() -> Vec<usize> {
+		loop {
+			let first_half = rand::thread_rng().gen_range(000..=998);
+			let second_half = rand::thread_rng().gen_range(0_000_000..=8_888_888);
+			let key: Vec<usize> = vec![first_half, second_half];
+
+			if validate_product_key(key.clone()) {
+				break key;
 			}
 		}
 	}
-}
-fn validate_product_key(product_key: &str) -> bool {
-	// Get first block from product key
-	let block_a: u32 = product_key[0..=2]
-		.parse()
-		.unwrap_or_default();
-	// Get second block from product key
-	let block_b: String = product_key[4..=10]
-		.parse()
-		.unwrap_or_default();
-	// Multiple conditions to check
-	if (product_key
-			.len() != 11
-		) ||
-		((3..=9)
-			.contains(&(block_a / 111)) &&
-			block_a % 111 == 0) ||
-		(product_key
-			.chars()
-			.nth(3) != Some('-')
-		) ||
-		(block_b
-			.to_string()
-			.chars()
-			.filter_map(|c| c
-				.to_digit(10)
-			)
-				.sum::<u32>() % 7 != 0) {
-		// Check fails if any of the conditions is true
-		return false;
-	} else {
-		// Check successful
-		return true;
+
+	fn validate_product_key(key: Vec<usize>) -> bool {
+		let first_three = key[0];
+		let last_seven = key[1];
+
+		// First half validation
+		if (3..=9).contains(&(first_three / 111)) && first_three % 111 == 0 {
+			return false;
+		}
+		// Second half validation
+		if second_check(last_seven) {
+			return true;
+		}
+		false
+	}
+
+	fn second_check(number: usize) -> bool {
+		let mut n = number;
+		let mut sum = 0;
+
+		while n > 0 {
+			if n % 10 == 9 {
+				return false; // If the last digit is 9, the number contains 9
+			}
+			sum += n % 10;
+			n /= 10; // Move to the next digit
+		}
+		if sum % 7 == 0 {
+			return true;
+		}
+		false
 	}
 }
